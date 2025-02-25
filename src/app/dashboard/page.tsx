f23e9f9d825a6387c7e32c155bc3951eb2b4a1dc
Meet <meetaparekh2003@gmail.com>
2025-02-25 13:27:46 +0530

    .

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
-import { ArrowRight } from 'lucide-react';
-import Link from 'next/link';
 
-type Props = {}
 
-const page = (props: Props) => {
+
+const page = () => {
   return (
     <div>
-        
+        page
     </div>
   )
 }
