680e028edf35c6f247afeff4603e3c6cad294a45
Aditya Singh <aditya.singh456m@gmail.com>
2025-03-11 15:12:02 +0530

    Update page.tsx

--- src/app/onBoarding/page.tsx
+++ src/app/onBoarding/page.tsx
@@ -72,7 +72,9 @@ const ReachUs = () => {
         portfolio: "",
       });
 
-      router.push("/dashboard");
+      sessionStorage.setItem("showSuccessToast", "true"); // ✅ Store flag for Snackbar
+      router.push("/"); // Redirect to Landing Page
+
     } catch (error) {
       console.error("Error:", error);
       alert("Failed to send message.");
@@ -136,18 +138,6 @@ const ReachUs = () => {
                 required
               />
             </div>
-            <div>
-              <label className="text-white block mb-2">Experience</label>
-              <textarea
-                name="experience"
-                value={formData.experience}
-                onChange={handleInputChange}
-                placeholder="Past-Project/ Experience"
-                rows={2}
-                className="w-full p-3 bg-gradient-to-r from-[rgba(70,48,104)] to-[rgba(163,103,228)] text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
-                required
-              ></textarea>
-            </div>
           </div>
           <div className="w-auto sm:w-1/2 px-14 sm:px-2">
             <form onSubmit={handleSubmit} className="space-y-6">
@@ -160,50 +150,6 @@ const ReachUs = () => {
                   onChange={handleInputChange}
                   placeholder="github.com/ username"
                   className="w-full p-2 bg-gradient-to-r from-[rgba(96,51,94)] to-[rgba(170,65,87)]   text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
-                />
-              </div>
-              <div>
-                <label className="text-white block mb-2">Twitter / X</label>
-                <input
-                  type="text"
-                  name="twitter"
-                  value={formData.twitter}
-                  onChange={handleInputChange}
-                  placeholder="x.com/ username"
-                  className="w-full p-2 bg-gradient-to-r from-[rgba(96,51,94)] to-[rgba(170,65,87)]   text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
-                />
-              </div>
-              <div>
-                <label className="text-white block mb-2">Linkedin</label>
-                <input
-                  type="text"
-                  name="linkedin"
-                  value={formData.linkedin}
-                  onChange={handleInputChange}
-                  placeholder="www.linkedin.com/in/ username"
-                  className="w-full p-2 bg-gradient-to-r from-[rgba(96,51,94)] to-[rgba(170,65,87)]   text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
-                />
-              </div>
-              <div>
-                <label className="text-white block mb-2">Portfolio/ Website</label>
-                <input
-                  type="text"
-                  name="portfolio"
-                  value={formData.portfolio}
-                  onChange={handleInputChange}
-                  placeholder="your link"
-                  className="w-full p-2 bg-gradient-to-r from-[rgba(96,51,94)] to-[rgba(170,65,87)]   text-white rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
-                />
-              </div>
-              <div>
-                <label className="text-white block mb-2">Wallet Address</label>
-                <input
-                  type="text"
-                  name="walletAddress"
-                  value={formData.walletAddress}
-                  onChange={handleInputChange}
-                  placeholder="0x"
-                  className="w-full p-2 text-white bg-gradient-to-r from-[rgba(96,51,94)] to-[rgba(170,65,87)]  rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
                 />
               </div>
               <button
